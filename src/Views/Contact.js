2b3aa0325b0a216ed3412b3360541e7890fa4616
elliewalkersm <elliewalkersm@gmail.com>
2021-05-29 09:21:14 -0500

    background color

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Contact() {
   return (
     <div className="contact-me">
-      <h2>Contact Me!</h2>
+      <h1>Contact Me!</h1>
       <a href="mailto: elliewalkersm@gmail.com">elliejaymeswalker@gmail.com</a>
     </div>
   );
